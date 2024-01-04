25ddc11d6078aa46ccdf9a89c9b0926395f22bd5
Daniele Trapani <daniele.trapani@outlook.it>
2024-01-04 21:19:31 +0100

    add tests to conversion

--- src/conversions.rs
+++ src/conversions.rs
@@ -12,3 +12,33 @@ impl ConvertibleToHnItem for rss::Item {
         Some(HnItem::new(title, snippet, guid))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_converts_rss_item_to_hn_item() {
+        let mut rss_item = rss::Item::default();
+        let mut guid = rss::Guid::default();
+        rss_item.set_title("title".to_string());
+        rss_item.set_description("snippet".to_string());
+        guid.set_value("00000000-0000-0000-0000-00000000000");
+        rss_item.set_guid(guid);
+
+        let hn_item = rss_item.convert_to_hn_item().unwrap();
+        assert_eq!(hn_item.title, "title");
+        assert_eq!(hn_item.snippet, "snippet");
+        assert_eq!(hn_item.guid, "00000000-0000-0000-0000-00000000000");
+    }
+
+    #[test]
+    fn it_fails_to_convert_rss_item_to_hn_item_when_something_is_missing() {
+        let mut rss_item = rss::Item::default();
+        let mut guid = rss::Guid::default();
+        rss_item.set_title("title".to_string());
+        guid.set_value("00000000-0000-0000-0000-00000000000");
+        rss_item.set_guid(guid);
+        let hn_item = rss_item.convert_to_hn_item();
+        assert_eq!(hn_item, None);
+    }
+}
